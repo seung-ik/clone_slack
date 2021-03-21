2f07317156fc9d34ee00a8010baa3ea23f9732f6
seung-ik <69374857+seung-ik@users.noreply.github.com>
2021-03-21 11:58:37 +0900

    signup err complete

--- client/layouts/App.tsx
+++ client/layouts/App.tsx
@@ -3,7 +3,7 @@ import {Switch, Route, Redirect} from "react-router-dom"
 import loadable from '@loadable/component';
 
 
-const Login = loadable(()=> import('../pages/Login'));
+const Login = loadable(()=> import('@pages/Login'));
 const SignUp = loadable(()=> import('@pages/SignUp')); 
 
 const App = () => {
